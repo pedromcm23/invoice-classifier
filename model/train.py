7d4ea988041408cbe66352b68c030772797c1cc0
Gabguiel <up202108816@g.uporto.pt>
2026-05-16 17:50:13 +0100

    Added possibility to correct the Category

--- model/train.py
+++ model/train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import joblib
+import json
 import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -12,10 +13,23 @@ from sklearn.metrics import classification_report
 from model.transformer import ColumnSelector
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "data", "faturas_sinteticas.csv")
+HISTORICO_PATH = os.path.join(os.path.dirname(__file__), "..", "data", "historico.json")
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "model.pkl")
 CATEGORIES_PATH = os.path.join(os.path.dirname(__file__), "categories.pkl")
 
+# Dataset Default
 df = pd.read_csv(DATA_PATH)
+# Correções do utilizador, caso existam
+if os.path.exists(HISTORICO_PATH):
+    with open(HISTORICO_PATH, "r", encoding="utf-8") as f:
+        historico_dados = json.load(f)
+    
+    if len(historico_dados) > 0:
+        df_historico = pd.DataFrame(historico_dados)
+
+        # Junta aos dados default
+        df = pd.concat([df, df_historico[["fornecedor", "descricao", "valor", "categoria"]]], ignore_index=True)
+        print(f"Adicionadas {len(df_historico)} faturas corrigidas pelo utilizador!")
 print(f"Dataset carregado: {len(df)} registos, {df['categoria'].nunique()} categorias\n")
 
 X = df[["fornecedor", "descricao", "valor"]]
